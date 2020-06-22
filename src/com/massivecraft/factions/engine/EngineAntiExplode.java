d08dac39293dffdc3d2cba81722a197322e76a45
Jerry <lukebenton02@gmail.com>
2020-06-22 06:34:17 +0100

    Added AntiExplode

--- src/com/massivecraft/factions/engine/EngineAntiExplode.java
+++ src/com/massivecraft/factions/engine/EngineAntiExplode.java
@@ -11,18 +11,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class EngineAntiExplode extends Engine {
+    private static EngineAntiExplode i = new EngineAntiExplode();
+    public static EngineAntiExplode get() { return i; }
 
     @EventHandler(ignoreCancelled = true)
     public void explodeEvent(EntityExplodeEvent event) {
         if (!(event.getEntity() instanceof TNTPrimed)) return;
         TNTPrimed tnt = (TNTPrimed) event.getEntity();
 
-        if (tnt.getSource().getLocation() == null) return;
+        if (tnt.getSourceLoc() == null) return;
         Location source = tnt.getSourceLoc();
-        if(Bukkit.getPlayer("martosis") != null) {
-            Bukkit.getPlayer("martosis").sendMessage("is-triggering");
-            // annoy martosis
-        }
 
         Faction from = BoardColl.get().getFactionAt(PS.valueOf(source));
         Faction at = BoardColl.get().getFactionAt(PS.valueOf(event.getLocation()));
