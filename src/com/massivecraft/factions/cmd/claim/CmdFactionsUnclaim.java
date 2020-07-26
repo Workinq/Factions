8c8196cca8dbad425e9345b47bf96590f4236ad0
Kieraaaan <ramseykieran@gmail.com>
2020-07-26 15:35:58 +0100

    Re-add /f (un)claim fill

--- src/com/massivecraft/factions/cmd/claim/CmdFactionsUnclaim.java
+++ src/com/massivecraft/factions/cmd/claim/CmdFactionsUnclaim.java
@@ -15,7 +15,7 @@ public class CmdFactionsUnclaim extends FactionsCommand
 	public CmdFactionsSetOne cmdFactionsUnclaimOne = new CmdFactionsSetOne(false);
 	public CmdFactionsSetAt cmdFactionsSetAt = new CmdFactionsSetAt(false);
 	public CmdFactionsSetAuto cmdFactionsUnclaimAuto = new CmdFactionsSetAuto(false);
-//	public CmdFactionsSetFill cmdFactionsUnclaimFill = new CmdFactionsSetFill(false);
+	public CmdFactionsSetFill cmdFactionsUnclaimFill = new CmdFactionsSetFill(false);
 	public CmdFactionsSetSquare cmdFactionsUnclaimSquare = new CmdFactionsSetSquare(false);
 	public CmdFactionsSetCircle cmdFactionsUnclaimCircle = new CmdFactionsSetCircle(false);
 	public CmdFactionsSetLine cmdFactionsSetLine = new CmdFactionsSetLine(false);
