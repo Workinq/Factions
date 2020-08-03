d0a36c62dfe71670bb0a65a83667f076992c0479
Kieraaaan <ramseykieran@gmail.com>
2020-08-03 14:03:37 +0100

    Reformat code from when Jerry changed it

--- src/com/massivecraft/factions/util/InventoryUtil.java
+++ src/com/massivecraft/factions/util/InventoryUtil.java
@@ -15,37 +15,38 @@ public class InventoryUtil {
     // TNT COUNTING
     // -------------------------------------------- //
 
-    public static int getTntIn(Inventory inventory) {
+    public static int getTntIn(Inventory inventory)
+    {
         int totalTnt = 0;
-        for (ItemStack item : inventory.getContents()) {
+        for (ItemStack item : inventory.getContents())
+        {
             if (item == null || item.getType() != Material.TNT) continue;
-
-            if (!item.hasItemMeta() && !item.getItemMeta().hasDisplayName() && !item.getItemMeta().hasLore()) {
-                totalTnt += item.getAmount();
-            }
+            if (!item.hasItemMeta() && !item.getItemMeta().hasDisplayName() && !item.getItemMeta().hasLore()) totalTnt += item.getAmount();
         }
         return totalTnt;
     }
 
-    public static int getOtherIn(Inventory inventory) {
+    public static int getOtherIn(Inventory inventory)
+    {
         int totalNotTnt = 0;
-        for (ItemStack item : inventory.getContents()) {
+        for (ItemStack item : inventory.getContents())
+        {
             if (item == null || item.getType() == Material.TNT) continue;
-
-            if (item.hasItemMeta() || item.getItemMeta().hasDisplayName() || item.getItemMeta().hasLore()) {
-                totalNotTnt += 64;
-            }
+            if (item.hasItemMeta() || item.getItemMeta().hasDisplayName() || item.getItemMeta().hasLore()) totalNotTnt += 64;
         }
         return totalNotTnt;
     }
 
-    public static void fillInventory(Inventory inventory) {
+    public static void fillInventory(Inventory inventory)
+    {
         fillInventory(inventory, new int[]{});
     }
 
-    public static void fillInventory(Inventory inventory, int[] slots) {
+    public static void fillInventory(Inventory inventory, int[] slots)
+    {
         ItemStack filler = new ItemBuilder(MConf.get().fillerItemMaterial).name(Txt.parse(MConf.get().fillerItemName)).durability(MConf.get().fillerItemData);
-        for (int i = 0; i < inventory.getSize(); i++) {
+        for (int i = 0; i < inventory.getSize(); i++)
+        {
             final int slot = i;
             if (Arrays.stream(slots).anyMatch(num -> num == slot)) continue;
             if (inventory.getItem(slot) != null) continue;
