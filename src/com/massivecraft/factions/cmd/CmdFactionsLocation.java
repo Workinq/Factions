968b447fb71612c6cde2d61e043a6ebab1728e75
Kieraaaan <ramseykieran@gmail.com>
2020-09-16 15:17:34 +0100

    Remove Faction parameter as it's not needed

--- src/com/massivecraft/factions/cmd/CmdFactionsLocation.java
+++ src/com/massivecraft/factions/cmd/CmdFactionsLocation.java
@@ -1,8 +1,6 @@
 package com.massivecraft.factions.cmd;
 
 import com.massivecraft.factions.cmd.req.ReqHasFaction;
-import com.massivecraft.factions.cmd.type.TypeFaction;
-import com.massivecraft.factions.entity.Faction;
 import com.massivecraft.factions.entity.MPerm;
 import com.massivecraft.massivecore.MassiveException;
 import org.bukkit.Location;
@@ -21,9 +19,6 @@ public class CmdFactionsLocation extends FactionsCommand
         // Desc
         this.setDescPermission("factions.location");
 
-        // Parameters
-        this.addParameter(TypeFaction.get(), "faction", "you");
-
         // Requirements
         this.addRequirements(ReqHasFaction.get());
     }
@@ -35,14 +30,11 @@ public class CmdFactionsLocation extends FactionsCommand
     @Override
     public void perform() throws MassiveException
     {
-        // Args
-        Faction faction = this.readArg(msenderFaction);
-
         // Verify
-        if ( ! MPerm.getPermLocation().has(msender, faction, true)) return;
+        if ( ! MPerm.getPermLocation().has(msender, msenderFaction, true)) return;
 
         Location at = me.getLocation();
-        faction.msg("%s <i>has pinged their location at x:<h>%,d <i>y:<h>%,d <i>z:<h>%,d <n>(<h>%s<n>)", msender.describeTo(faction, true), at.getBlockX(), at.getBlockY(), at.getBlockZ(), at.getWorld().getName());
+        msenderFaction.msg("%s <i>has pinged their location at x:<h>%,d <i>y:<h>%,d <i>z:<h>%,d <n>(<h>%s<n>)", msender.describeTo(msenderFaction, true), at.getBlockX(), at.getBlockY(), at.getBlockZ(), at.getWorld().getName());
     }
 
 }
