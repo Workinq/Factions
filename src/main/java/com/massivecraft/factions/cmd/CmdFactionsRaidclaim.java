501dda534ba8e59468c2af187fbaef93a28a0c19
Kieran Ramsey <ramseykieran@gmail.com>
2026-06-21 19:54:09 -0400

    Reuse the regular claim subcommands for raid claims so one at square circle corner line and fill all work

--- src/main/java/com/massivecraft/factions/cmd/CmdFactionsRaidclaim.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdFactionsRaidclaim.java
@@ -2,18 +2,10 @@ package com.massivecraft.factions.cmd;
 
 import com.massivecraft.factions.ClaimType;
 import com.massivecraft.factions.Perm;
-import com.massivecraft.factions.entity.BoardColl;
-import com.massivecraft.factions.entity.Faction;
-import com.massivecraft.factions.entity.MConf;
 import com.massivecraft.massivecore.MassiveException;
 import com.massivecraft.massivecore.command.requirement.RequirementHasPerm;
-import com.massivecraft.massivecore.command.requirement.RequirementIsPlayer;
-import com.massivecraft.massivecore.ps.PS;
-import com.massivecraft.massivecore.util.TimeDiffUtil;
-import com.massivecraft.massivecore.util.TimeUnit;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
+import java.util.ArrayList;
 
 public class CmdFactionsRaidclaim extends FactionsCommand
 {
@@ -21,6 +13,13 @@ public class CmdFactionsRaidclaim extends FactionsCommand
 	// FIELDS
 	// -------------------------------------------- //
 
+	public CmdFactionsSetOne cmdFactionsRaidclaimOne = new CmdFactionsSetOne(true);
+	public CmdFactionsSetAt cmdFactionsRaidclaimAt = new CmdFactionsSetAt(true);
+	public CmdFactionsSetFill cmdFactionsRaidclaimFill = new CmdFactionsSetFill(true);
+	public CmdFactionsSetSquare cmdFactionsRaidclaimSquare = new CmdFactionsSetSquare(true);
+	public CmdFactionsSetCircle cmdFactionsRaidclaimCircle = new CmdFactionsSetCircle(true);
+	public CmdFactionsSetCorner cmdFactionsRaidclaimCorner = new CmdFactionsSetCorner(true);
+	public CmdFactionsSetLine cmdFactionsRaidclaimLine = new CmdFactionsSetLine(true);
 	public CmdFactionsRaidclaimRemove cmdFactionsRaidclaimRemove = new CmdFactionsRaidclaimRemove();
 
 	// -------------------------------------------- //
@@ -29,8 +28,20 @@ public class CmdFactionsRaidclaim extends FactionsCommand
 
 	public CmdFactionsRaidclaim()
 	{
-		this.addRequirements(RequirementIsPlayer.get());
-		this.addRequirements(RequirementHasPerm.get(Perm.RAIDCLAIM));
+		CmdFactionsSetX[] variants = {
+			this.cmdFactionsRaidclaimOne,
+			this.cmdFactionsRaidclaimAt,
+			this.cmdFactionsRaidclaimFill,
+			this.cmdFactionsRaidclaimSquare,
+			this.cmdFactionsRaidclaimCircle,
+			this.cmdFactionsRaidclaimCorner,
+			this.cmdFactionsRaidclaimLine,
+		};
+		for (CmdFactionsSetX variant : variants)
+		{
+			variant.setClaimType(ClaimType.RAID);
+			variant.addRequirements(RequirementHasPerm.get(Perm.RAIDCLAIM));
+		}
 	}
 
 	// -------------------------------------------- //
@@ -40,19 +51,7 @@ public class CmdFactionsRaidclaim extends FactionsCommand
 	@Override
 	public void perform() throws MassiveException
 	{
-		if ( ! MConf.get().raidClaimsEnabled) throw new MassiveException().setMsg("<b>Raid claims are disabled.");
-
-		Faction faction = msenderFaction;
-		if ( ! faction.isNormal()) throw new MassiveException().setMsg("<b>You must be in a faction to place a raid claim.");
-
-		PS chunk = PS.valueOf(me.getLocation()).getChunk(true);
-		if ( ! BoardColl.get().getFactionAt(chunk).isNone()) throw new MassiveException().setMsg("<b>Raid claims can only be placed on wilderness.");
-
-		long expiryMillis = System.currentTimeMillis() + MConf.get().raidClaimExpiryMillis;
-		if ( ! msender.tryClaim(faction, Collections.singleton(chunk), null, null, ClaimType.RAID, expiryMillis)) return;
-
-		LinkedHashMap<TimeUnit, Long> unitcounts = TimeDiffUtil.limit(TimeDiffUtil.unitcounts(MConf.get().raidClaimExpiryMillis, TimeUnit.getAllButMillis()), 2);
-		msg("<i>This raid claim will expire in <h>%s<i>.", TimeDiffUtil.formatedMinimal(unitcounts, "<h>"));
+		this.cmdFactionsRaidclaimOne.execute(sender, new ArrayList<>());
 	}
 
 }
--- src/main/java/com/massivecraft/factions/cmd/CmdFactionsSetX.java
+++ src/main/java/com/massivecraft/factions/cmd/CmdFactionsSetX.java
@@ -1,8 +1,12 @@
 package com.massivecraft.factions.cmd;
 
+import com.massivecraft.factions.ClaimType;
+import com.massivecraft.factions.entity.BoardColl;
 import com.massivecraft.factions.entity.Faction;
 import com.massivecraft.factions.entity.FactionColl;
+import com.massivecraft.factions.entity.MConf;
 import com.massivecraft.massivecore.MassiveException;
+import com.massivecraft.massivecore.collections.MassiveSet;
 import com.massivecraft.massivecore.ps.PS;
 
 import java.util.Set;
@@ -24,6 +28,10 @@ public abstract class CmdFactionsSetX extends FactionsCommand
 	private boolean claim = true;
 	public boolean isClaim() { return this.claim; }
 	public void setClaim(boolean claim) { this.claim = claim; }
+
+	private ClaimType claimType = ClaimType.NORMAL;
+	public ClaimType getClaimType() { return this.claimType; }
+	public void setClaimType(ClaimType claimType) { this.claimType = claimType; }
 	
 	private int factionArgIndex = 0;
 	public int getFactionArgIndex() { return this.factionArgIndex; }
@@ -48,10 +56,31 @@ public abstract class CmdFactionsSetX extends FactionsCommand
 	{	
 		// Args
 		final Faction newFaction = this.getNewFaction();
-		final Set<PS> chunks = this.getChunks();
+		Set<PS> chunks = this.getChunks();
+
+		Long expiryMillis = null;
+		if (this.getClaimType() == ClaimType.RAID)
+		{
+			if (chunks == null) return;
+			if ( ! MConf.get().raidClaimsEnabled) throw new MassiveException().setMsg("<b>Raid claims are disabled.");
+			chunks = this.filterWilderness(chunks);
+			if (chunks.isEmpty()) throw new MassiveException().setMsg("<b>Raid claims can only be placed on wilderness.");
+			expiryMillis = System.currentTimeMillis() + MConf.get().raidClaimExpiryMillis;
+		}
 		
 		// Apply / Inform
-		msender.tryClaim(newFaction, chunks, this.getFormatOne(), this.getFormatMany());
+		msender.tryClaim(newFaction, chunks, this.getFormatOne(), this.getFormatMany(), this.getClaimType(), expiryMillis);
+	}
+
+	private Set<PS> filterWilderness(Set<PS> chunks)
+	{
+		Set<PS> ret = new MassiveSet<>();
+		for (PS chunk : chunks)
+		{
+			if ( ! BoardColl.get().getFactionAt(chunk).isNone()) continue;
+			ret.add(chunk);
+		}
+		return ret;
 	}
 	
 	// -------------------------------------------- //
